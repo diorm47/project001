02df623d07d705e637b6f568363f23805d329e85
diorm47 <dior47m@gmail.com>
2023-11-17 21:48:15 +0500

    roulette edits

--- src/components/case-opening/case-opening.jsx
+++ src/components/case-opening/case-opening.jsx
@@ -6,103 +6,169 @@ import { ReactComponent as GradientLeft } from "../../assets/icons/case-opening-
 import { ReactComponent as GradientRight } from "../../assets/icons/case-opening-right-gr.svg";
 import case_item_img from "../../assets/images/case-item.png";
 import sound from "../../assets/sound.mp3";
-import RoulettePro from "react-roulette-pro";
+import winnedAudio from "../../assets/winned.mp3";
+
+import useSound from "use-sound";
 import "react-roulette-pro/dist/index.css";
+import { generateRandomNumber } from "../utils/utils";
+import spin_bg from "../../assets/images/updating-bg.png";
 
 function CaseOpening() {
   const case_items = [
     {
-      id: 567890,
+      id: 1111,
       description: "789 кристаллов 💎",
       cost: "23",
       image: case_item_img,
       rarity: 11,
     },
     {
-      id: 567781,
+      id: 2222,
       description: "679 кристаллов 💎",
       cost: "566",
       image: case_item_img,
       rarity: 12,
     },
     {
-      id: 87999981,
+      id: 3333,
       description: "568 кристаллов 💎",
       cost: "567",
       image: case_item_img,
       rarity: 13,
     },
     {
-      id: 5634,
+      id: 4444,
       description: "89 кристаллов 💎",
       cost: "5678",
       image: case_item_img,
       rarity: 1,
     },
     {
-      id: 567678,
+      id: 5555,
       description: "23 кристаллов 💎",
       cost: "5688",
       image: case_item_img,
       rarity: 13,
     },
     {
-      id: 56789,
+      id: 6666,
       description: "12 кристаллов 💎",
       cost: "5688",
       image: case_item_img,
       rarity: 18,
     },
     {
-      id: 56799,
+      id: 7777,
       description: "665 кристаллов 💎",
       cost: "5688",
       image: case_item_img,
       rarity: 12,
     },
     {
-      id: 678892,
+      id: 8888,
       description: "10 кристаллов 💎",
       cost: "5644",
       image: case_item_img,
       rarity: 13,
     },
     {
-      id: 34345,
+      id: 9999,
       description: "340 кристаллов 💎",
       cost: "5678",
       image: case_item_img,
       rarity: 15,
     },
   ];
+  const extendedItems = [
+    ...case_items,
+    ...case_items,
+    ...case_items,
+    ...case_items,
+  ];
   const wheelRef = useRef(null);
   const [isSpinning, setIsSpinning] = useState(false);
+  const [winnedPrize, setWinnedPrize] = useState({
+    id: 1111,
+    description: "789 кристаллов 💎",
+    cost: "23",
+    image: case_item_img,
+    rarity: 11,
+  });
+  const [winnedPrizeBlock, setWinnedPrizeBlock] = useState(false);
+
+  const [play, { stop }] = useSound(sound, { volume: 0.5 });
+  const [playWinnedSound, { stop: stopWinnedSound }] = useSound(winnedAudio, {
+    volume: 0.5,
+  });
+
+  const resetWheel = () => {
+    if (wheelRef.current) {
+      wheelRef.current.style.transition = "none";
+      wheelRef.current.style.transform = "translateX(0)";
+    }
+    setIsSpinning(false);
+  };
 
   const spinWheel = (selectedId) => {
+    setWinnedPrizeBlock(false);
     setIsSpinning(true);
-    const selectedIndex = case_items.findIndex((item) => item.id === selectedId);
+    const selectedIndex = case_items.findIndex(
+      (item) => item.id === selectedId
+    );
     if (selectedIndex === -1) {
-      console.error("Selected item not found");
       return;
     }
-  
-    // Вычисляем расстояние для вращения
+
+    setWinnedPrize(case_items.filter((item) => item.id === selectedId));
     const cardWidth = 183;
-    const offset = (window.innerWidth - cardWidth) / 2;
-    const initialSpin = cardWidth * case_items.length * 2; // начальное вращение для эффекта
+    const viewportCenter = window.innerWidth / 2;
+    const initialSpin = cardWidth * case_items.length * 2;
     const targetPosition = cardWidth * selectedIndex;
-    const spinDistance = initialSpin + targetPosition - offset;
-  
-    // Применяем стиль для анимации
-    wheelRef.current.style.transition = "transform 10s cubic-bezier(0.33, 1, 0.68, 1)";
-    wheelRef.current.style.transform = `translateX(-${spinDistance}px)`;
-  
+    const centeringOffset = cardWidth / 2;
+    const spinDistance =
+      initialSpin + targetPosition + centeringOffset - viewportCenter;
+    wheelRef.current.style = "filter: blur(0);";
+    wheelRef.current.style.transition =
+      "transform 10s cubic-bezier(0.25, 1, 0.40, 1)";
+    wheelRef.current.style.transform = `translateX(-${
+      spinDistance + cardWidth + generateRandomNumber()
+    }px)`;
+
+    let previousCardEdge = null;
+    let interval = setInterval(() => {
+      const currentTransform = wheelRef.current.style.transform;
+      const currentX = parseFloat(
+        currentTransform.split("(")[1].split("px")[0]
+      );
+      const currentCardEdge = Math.abs(currentX % cardWidth);
+
+      previousCardEdge = currentCardEdge;
+    }, 100);
+
     setTimeout(() => {
       setIsSpinning(false);
+      clearInterval(interval);
+
+      const finalAdjustment = spinDistance + cardWidth / 2;
+      wheelRef.current.style.transition = "transform 0.5s ease-out";
+      wheelRef.current.style.transform = `translateX(-${
+        finalAdjustment + cardWidth
+      }px)`;
+
+      setTimeout(() => {
+        setWinnedBlock();
+      }, 700);
+      setTimeout(() => {
+        resetWheel();
+      }, 3000);
     }, 10000);
   };
-  
-  const extendedItems = [...case_items, ...case_items, ...case_items];
+
+  const setWinnedBlock = () => {
+    wheelRef.current.style = "filter: blur(5px);";
+    setWinnedPrizeBlock(true);
+    playWinnedSound();
+  };
 
   return (
     <>
@@ -140,11 +206,48 @@ function CaseOpening() {
           </div>
         </div>
         <div className="case_opening_process_btn">
-          <button onClick={() => spinWheel(5634)}>
+          <button onClick={() => spinWheel(1111)}>
             <p>ОТКРЫВАЕТСЯ</p> <GifLoader />
           </button>
         </div>
       </div>
+
+      {winnedPrizeBlock ? (
+        <div className="spin_result">
+          <div className="spin_result_wrapper">
+            {winnedPrize ? (
+              <div className="spin_result_item">
+                <div className="spin_result_item_image">
+                  <img src={spin_bg} className="spin_result_item_bg" alt="" />
+                  <img
+                    src={case_item_img}
+                    className="spin_result_item-image"
+                    alt=""
+                  />
+                </div>
+                <div className="spin_result_item_descriptions">
+                  <div className="spin_result_item_description">
+                    <p>{winnedPrize[0].description}</p>
+                  </div>
+                  <div className="spin_result_item_cost">
+                    <p>{winnedPrize[0].cost} ₽</p>
+                  </div>
+                  <div className="spin_result_item_id">
+                    <h2>ID: {winnedPrize[0].id}</h2>
+                  </div>
+                  <div className="retry_btn">
+                    <button onClick={() => spinWheel(1111)}>Ещё раз</button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -12,3 +12,22 @@ export const saveAsTxtFile = (filename, text) => {
   link.click();
   URL.revokeObjectURL(link.href);
 };
+
+export const generateRandomNumber = () => {
+  const lowerRange1 = 20;
+  const upperRange1 = 40;
+  const lowerRange2 = 100;
+  const upperRange2 = 165;
+
+  const randomRange = Math.random() < 0.5 ? 1 : 2;
+
+  if (randomRange === 1) {
+    return (
+      Math.floor(Math.random() * (upperRange1 - lowerRange1 + 1)) + lowerRange1
+    );
+  } else {
+    return (
+      Math.floor(Math.random() * (upperRange2 - lowerRange2 + 1)) + lowerRange2
+    );
+  }
+};
